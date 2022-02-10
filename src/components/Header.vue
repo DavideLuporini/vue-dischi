7527d66eff97e8241314abb9aebdee8c777f7b4c
illupovero <davideluporini1996@gmail.com>
2022-02-10 16:43:53 +0100

    added bootstap + header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
 <header>
-
+<nav class="navbar navbar-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img class="img-fluid" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="" width="30" height="24">
+    </a>
+  </div>
+</nav>
 </header>
 </template>
 
@@ -15,4 +21,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import 'bootstrap';
+nav{
+  background-color: #2e3a46;
+  img{
+    height: 35px;
+      width: 95px;
+  
+  }
+}
 </style>
